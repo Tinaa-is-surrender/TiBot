e6a5d8eb2de511815f5cf6f89d98d2a2eb7c342e
Glitch (bot22221) <none>
2021-10-30 12:09:34 +0000

    ✋🐺 Checkpoint ./bot.js:37225228/6

--- bot.js
+++ bot.js
@@ -153,7 +153,7 @@ function Makgora(channel, tags, message, self) {
 
 /*client.on('message', (channel, tags, message, self) => {
 	if(self || !message.startsWith('!')) return;
-
+client
 	const args = message.slice(1).split(' ');
 	const command = args.shift().toLowerCase();
 
