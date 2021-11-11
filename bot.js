71f6e8fa57d4bcf27760c14ca76633599b882def
Glitch (bot22221) <none>
2021-11-11 18:04:39 +0000

    🍜⭐️ Checkpoint ./bot.js:37225228/148

--- bot.js
+++ bot.js
@@ -35,7 +35,8 @@ var days = Math.floor(streamperiod / (1000 * 60 * 60 * 24));
 client.on("chat", function(channel, user, message, self) {
   if (message.startsWith("!카드")) {
     var axios = require("axios").default;
-    var card = message.split(" ")[1];
+    var cardname = message.split(" ")[1];
+    var card = encodeURIComponent(cardname)
     var options = {
       method: "GET",
       url: `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${card}`,
@@ -55,7 +56,7 @@ client.on("chat", function(channel, user, message, self) {
         console.error(error);
       });
     if (card == null) client.say(channel, "검색할 카드를 입력해주세요");
-    else client.say(channel, `${card}에 대한 정보를 찾았어요!`);
+    else client.say(channel, `${cardname}에 대한 정보를 찾았어요!`);
     client.say(channel, `네요`);
   }
 });
